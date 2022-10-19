35fc8786018a8c6ddc5bfbc22143a5198cce4499
Jim <jim@jimtufts.com>
2022-10-19 07:57:41 -0500

    Add test for BindingSite init

--- occupancy_fingerprinter/tests/test_occupancy_fingerprinter.py
+++ occupancy_fingerprinter/tests/test_occupancy_fingerprinter.py
@@ -17,14 +17,18 @@ def test_occupancy_fingerprinter_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "occupancy_fingerprinter" in sys.modules
 
-def test_binding_site():
+def test_binding_site_init():
     """Test binding site init"""
     center = np.array([10.,10.,10.])
     r = 5.
     spacing = np.array([1., 1., 1.])
     b = BindingSite(center, r, spacing)
-    assert b._center == center
+    assert (b._center == center).all()
     assert b._r == r 
-    assert b._spacing == spacing
-    assert b._counts == b.get_grid_counts()
-    assert b._origin == b.get_origin()
+    assert (b._spacing == spacing).all()
+    assert (b._counts == b.get_grid_counts()).all()
+    assert (b._origin == b.get_origin()).all()
+    assert (b._upper_most_corner_crd == (self._center + ((self._counts - 1) * self._spacing)/2)).all()
+    assert (b._upper_most_corner == (b._counts - 1)).all()
+    assert (b._size == np.prod(b._counts))
+
