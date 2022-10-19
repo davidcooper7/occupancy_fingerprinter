e128403555896799c3c24cf3ce71a2490d5a138c
Jim <jim@jimtufts.com>
2022-10-19 17:23:27 -0500

    Fix final typo in Grid init test.

--- occupancy_fingerprinter/tests/test_occupancy_fingerprinter.py
+++ occupancy_fingerprinter/tests/test_occupancy_fingerprinter.py
@@ -56,7 +56,7 @@ def test_grid_init():
     g.add_binding_site(center, r, spacing)
     b = BindingSite(center,r,spacing)
     assert g._n_sites == 1
-    assert g._sites[0]._center == b._center
+    assert (g._sites[0]._center == b._center).all()
     assert g._sites[0]._r == b._r
     assert (g._sites[0]._spacing == b._center._spacing).all()
 
